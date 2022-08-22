28b72bd867c645a8714d26bcbd6d7bee8cba0151
AzyaFLEX <AzyaDr@yandex.ru>
2022-08-22 03:25:30 +0300

    alpha - version: 1.1
    Added a "find_connection" - function that allow user to find social chain with vk

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from functools import lru_cache
 import configparser
 import vk_api
 
@@ -47,7 +48,53 @@ def show_list(_vk, _data: dict) -> None:
         print(f'{index + 1}. {get_group_name(_vk, elm)} - {_data[elm]}')
 
 
+def get_top_publics():
+    _config = get_config('config.ini')
+    _vk = create_session(_config)
+    show_list(_vk, get_public_list(_vk, get_chat_users(_vk, chat_id=_config['group']['id'])))
+
+
+@lru_cache()
+def get_users_name_by_id(_vk, user_id):
+    data = _vk.users.get(user_ids=user_id)[0]
+    return f'{data["first_name"]} {data["last_name"]} ({user_id})'
+
+
+def find_connection(_vk, main_id, start_id, white_list: list = None, black_list: list = None, max_length=4):
+    def print_chain(_chain: list):
+        print(" -> ".join(map(lambda id: get_users_name_by_id(_vk, id), _chain + [main_id])))
+
+    @lru_cache()
+    def get_user_friends(id):
+        return _vk.friends.get(user_id=id)['items']
+
+    queue = [[start_id]]
+    check = _vk.friends.get(user_id=main_id)['items']
+
+    if black_list is None:
+        black_list = []
+    if white_list is None:
+        white_list = []
+
+    if start_id in check:
+        print(f'{get_users_name_by_id(_vk, start_id)} -> {get_users_name_by_id(_vk, main_id)}')
+        check.remove(start_id)
+
+    while queue and len(min(queue, key=len)) <= max_length:
+        for chain in queue[:]:
+            try:
+                for user_id in get_user_friends(chain[-1]):
+                    if user_id not in chain and user_id not in black_list:
+                        if user_id in check and (True if not white_list else user_id in white_list):
+                            print_chain(chain + [user_id])
+                        if user_id != main_id:
+                            queue.append(chain + [user_id])
+            except vk_api.exceptions.ApiError:
+                pass
+            queue.remove(chain)
+
+
 if __name__ == '__main__':
     config = get_config('config.ini')
     vk = create_session(config)
-    show_list(vk, get_public_list(vk, get_chat_users(vk, chat_id=config['group']['id'])))
+    find_connection(vk, 291642276, 250802218)
